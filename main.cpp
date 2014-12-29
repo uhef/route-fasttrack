dd4668175446f2e984a76ef9ac7672944cb08643
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-12-29 16:10:23 +0200

    Open graph.json for reading data in

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cstdio>
 
 int main() {
   std::cout << "Hello world!" << std::endl;
+  FILE* inputFile = fopen("graph.json", "r");
+  if (inputFile != NULL) {
+    std::cout << "File open successfull" << std::endl;
+    fclose(inputFile);
+  }
   return 0;
 }
 
