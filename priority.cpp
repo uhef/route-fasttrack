67041ce1ee9287499284cc2c2e06168f78e0eb9e
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-12-30 17:25:34 +0200

    Add class to be used to instantiate elements in priority queue

--- priority.cpp
+++ priority.cpp
@@ -2,8 +2,23 @@
 #include <queue>
 #include <string>
 
+class Element {
+public:
+  std::string name;
+  int priority;
+  Element(const std::string& n, int p) {
+    name = n;
+    priority = p;
+  }
+  const std::string& toString() {
+    return name;
+  }
+};
+
 int main() {
   std::cout << "Hello priority queue!" << std::endl;
+  Element e("foobar", 10);
+  std::cout << e.toString() << std::endl;
   std::priority_queue<int> pq;
   pq.push(3);
   pq.push(6);
