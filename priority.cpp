c414b0649ed684a038aaaf372a27ee3d101ff56c
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-12-30 18:16:12 +0200

    Store elements in priority queue

--- priority.cpp
+++ priority.cpp
@@ -10,8 +10,16 @@ public:
     name = n;
     priority = p;
   }
-  const std::string& toString() {
+  const std::string& toString() const {
     return name;
+  }
+};
+
+class ElementComparison
+{
+public:
+  bool operator() (const Element& left, const Element& right) const {
+    return (left.priority < right.priority);
   }
 };
 
@@ -19,6 +27,16 @@ int main() {
   std::cout << "Hello priority queue!" << std::endl;
   Element e("foobar", 10);
   std::cout << e.toString() << std::endl;
+
+  std::priority_queue<Element, std::vector<Element>, ElementComparison> elementQueue;
+  elementQueue.push(Element("foo", 3));
+  elementQueue.push(Element("bar", 6));
+  elementQueue.push(Element("foz", 1));
+  while(!elementQueue.empty()) {
+    std::cout << "Next element: " << elementQueue.top().toString() << std::endl;
+    elementQueue.pop();
+  }
+
   std::priority_queue<int> pq;
   pq.push(3);
   pq.push(6);
