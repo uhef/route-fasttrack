38d7960ba5af04cee6a4faa2bea7b227e3843605
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2014-12-31 10:32:11 +0200

    Add rapidjson and example on usage

---
+++ rapidjson.cpp
@@ -0,0 +1,23 @@
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+
+using namespace rapidjson;
+
+int main() {
+  const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+  Document d;
+  d.Parse(json);
+
+  Value& s = d["stars"];
+  s.SetInt(s.GetInt() + 1);
+
+  StringBuffer buffer;
+  Writer<StringBuffer> writer(buffer);
+  d.Accept(writer);
+
+  std::cout << buffer.GetString() << std::endl;
+  return 0;
+}
+
