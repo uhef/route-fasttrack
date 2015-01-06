a367965a6ba419f6aa5425940d1e37177af6a77b
uhef <tuomas.jarvensivu@iki.fi>
2015-01-06 21:28:17 +0200

    Insert custom weight objects to unordered map

--- main.cpp
+++ main.cpp
@@ -30,13 +30,16 @@ int main() {
 
   std::cout << d.IsArray() << std::endl;
 
-  std::unordered_map<int, int> weights;
+  std::unordered_map<int, Weight> weights;
   for (SizeType i = 0; i < d.Size(); ++i) {
-    weights[d[i]["from"].GetInt()] = d[i]["to"].GetInt();
+    int source = d[i]["from"].GetInt();
+    int target = d[i]["to"].GetInt();
+    int weight = d[i]["weight"].GetInt();
+    weights.emplace(source, Weight(target, weight));
   }
-  std::cout << "Node 0 leads to " << weights[0] << std::endl;
-  std::cout << "Node 1 leads to " << weights[1] << std::endl;
-  std::cout << "Node 2 leads to " << weights[2] << std::endl;
+  std::cout << "Node 0 leads to " << weights.at(0).node << " by weight " << weights.at(0).weight << std::endl;
+  std::cout << "Node 1 leads to " << weights.at(1).node << " by weight " << weights.at(1).weight << std::endl;
+  std::cout << "Node 2 leads to " << weights.at(2).node << " by weight " << weights.at(2).weight << std::endl;
 
   return 0;
 }
